38d94f2b284cafd1a70883853c7233673bf8c8a9
unknown <akinjejikolawoleraphael@gmail.com>
2025-01-30 12:37:25 +0100

    files upload

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+  const response = {
+    email: "akinjejikolawoleraphael@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_URL: "https://github.com/Kollarztune1/stage-0-public-API",
+  };
+
+  res.json (response);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
